e2afcb1bfc863d4b806de62124c111c89f05929c
zaolee <lee970808@naver.com>
2022-12-09 16:51:04 +0900

    IO기반압출력및네트워킹

--- chap18/src/sec08/exam01_udp/UdpReceiveExample.java
+++ chap18/src/sec08/exam01_udp/UdpReceiveExample.java
@@ -6,6 +6,7 @@ import java.net.DatagramSocket;
 public class UdpReceiveExample extends Thread {
 	public static void main(String[] args) throws Exception {
 		DatagramSocket datagramSocket = new DatagramSocket(5001);
+		// 데이타를 받는(수신자)쪽의 코드
 		
 		Thread thread = new Thread() {
 			@Override
@@ -30,3 +31,35 @@ public class UdpReceiveExample extends Thread {
 		datagramSocket.close();
 	}
 }
+//import java.net.InetSocketAddress;
+//
+//public class UdpSendExample {
+//	public static void main(String[] args) throws Exception {
+//		// 데이타를 보내는(발신자)쪽의 코드
+//		DatagramSocket datagramSocket = new DatagramSocket(5001);
+//		
+//		// 작업스레드 생성 
+//		Thread thread = new Thread() { // 익명자식객체로 만들꺼
+//			@Override
+//			public void run() {
+//				System.out.println("[수신 시작]");
+//				try {
+//					while(true) {
+//						DatagramPacket packet = new DatagramPacket(new byte[100],100);
+//						datagramSocket.receive(packet); // 데이터 입력될때까지 기다림.
+//						
+//						String data = new String(packet.getData(),0,packet.getLength(),"UTF-8"); // 디코딩(byte->String)
+//						System.out.println("[받은 내용: " + packet.getSocketAddress() + "]");
+//					}
+//				} catch(Exception e) {
+//					System.out.println("[수신 종료]");
+//				}
+//			} 			
+//		};
+//		thread.start(); // 작업스레드를 병렬로 실행시킨다.
+//		
+//		thread.sleep(10000); // 메일스레드를 일시정지상태로 만든다. 
+//		datagramSocket.close();
+//	}
+//}
+
--- chap18/src/sec08/exam01_udp/UdpSendExample.java
+++ chap18/src/sec08/exam01_udp/UdpSendExample.java
@@ -6,24 +6,31 @@ import java.net.InetSocketAddress;
 
 public class UdpSendExample {
 	public static void main(String[] args) throws Exception {
-		DatagramSocket datagramSocket = new DatagramSocket();
-		
-		System.out.println("[발신 시작]");
-		
-		for(int i=1; i<3; i++) {
-			String data = "메시지" + i;
-			byte[] byteArr = data.getBytes("UTF-8");
-			DatagramPacket packet = new DatagramPacket(
-				byteArr, byteArr.length, 
-				new InetSocketAddress("localhost", 5001)
-			);
-			
-			datagramSocket.send(packet);
-			System.out.println("[보낸 바이트 수]: " + byteArr.length + " bytes");
-		}
-		
-		System.out.println("[발신 종료]");
-		
-		datagramSocket.close();
+		// 데이타를 보내는(발신자)쪽의 코드
+	      DatagramSocket datagramSocket = new DatagramSocket();
+	      System.out.println("[발신 시작]");
+	      
+//	      for 문이용
+	      for(int i=1; i<3; i++) {
+
+	         String data = "메시지" + i+"from 소연";
+
+	         byte[] byteArr = data.getBytes("UTF-8");//UTF-8은 가변용 문자셋
+
+	         DatagramPacket packet = new DatagramPacket(
+	            byteArr, byteArr.length, //생성자에 매개변수 3개 있음
+	            new InetSocketAddress("localhost", 5001)
+	            //자동으로 누가 보내는지는 생성되게된다????????
+	         );
+	         
+
+	         datagramSocket.send(packet);
+	         System.out.println("[보낸 바이트 수]: " + byteArr.length + " bytes");
+
+	      }
+	      
+	      System.out.println("[발신 종료]");
+
+	      datagramSocket.close();
+	   }
 	}
-}
